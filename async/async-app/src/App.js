81bca5b3915f1e5bdd14f44ba7a5b14bef142135
Ritheesh <ritheesh.4@gmail.com>
2020-09-28 15:21:31 +0530

    asyn exmample

---
+++ async/async-app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState({meaningOfLife: this.state.meaningOfLife + 1})
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button
+          onClick={this.handleClick}
+          >
+            Update state
+        </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
